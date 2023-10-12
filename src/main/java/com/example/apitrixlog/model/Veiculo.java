71fcadc8a94c3fe1c50d1d721f33e6264d309d0d
RaulRamos94 <raulfernandes3@hotmail.com>
2023-10-12 11:58:58 -0300

    criação MODEL CONDUTOR E VEICULO

---
+++ src/main/java/com/example/apitrixlog/model/Veiculo.java
@@ -0,0 +1,47 @@
+package com.example.apitrixlog.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "veiculo")
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 50)
+    private String marca;
+
+    @Column(nullable = false, length = 10)
+    private String placa;
+
+    @Pattern(regexp = "^(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO|BR)$", message= "Estado inválido")
+    @Column(nullable = false, length = 2)
+    private String ufPlaca;
+
+    @Column(nullable = false, length = 50)
+    private int renavan;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(nullable = false, length = 10)
+    private String dataAquisicao;
+
+}
---
+++ src/test/java/com/example/apitrixlog/ApiTrixlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apitrixlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrixlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
