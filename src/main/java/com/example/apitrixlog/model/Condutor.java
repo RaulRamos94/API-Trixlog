f639f9f195b2599c741d020c6dcf295aa411aeb2
RaulRamos94 <raulfernandes3@hotmail.com>
2023-10-13 08:58:13 -0300

    alteracao na relacao das tabelas

--- src/main/java/com/example/apitrixlog/model/Condutor.java
+++ src/main/java/com/example/apitrixlog/model/Condutor.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,7 +26,6 @@ public class Condutor {
     private String nome;
 
     @OneToMany
-    @JoinColumn(name = "idVeiculo")
     private List<Veiculo> veiculos;
     
 }
